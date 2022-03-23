a1edcfe4298c881b521835c479ca01d227cb64aa
alisherbegmatov <alisher.begmatov@students.dominican.edu>
2022-03-23 16:10:40 -0700

    Challenge 3 Completed
    Challenge 3 Completed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const { engine } = require('express-handlebars');
+const fetch = require('node-fetch');
 const app = express();
 
 app.engine('handlebars', engine());
@@ -8,7 +10,18 @@ app.set("views", "./views");
 
 app.get('/', 
   (req, res) => {
-    res.render('home');
+    let term = "";
+    if (req.query.term) {
+      term = req.query.term
+    }
+    fetch(`https://g.tenor.com/v1/search?q=${term}&key=${process.env.API_KEY}&limit=10`)
+    .then(response => response.json())
+    .then(
+      (data) => {
+        const gifs = data.results;
+        res.render('home', { gifs });
+      }
+    );
   }
 );
 
